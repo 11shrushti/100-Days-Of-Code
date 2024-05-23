be3b94014aadf0fc54a077e7d67cc685d4c48728
11shrushti <shrushtirokade05@gamil.com>
2024-05-23 16:03:32 +0530

    quiz game

---
+++ quiz_game/question_model.py
@@ -0,0 +1,5 @@
+# TODO1 : create a Question class with an __init__ method with tw oattributes : text and answer
+class Question:
+    def __init__(self, q_text,q_answer):
+        self.text = q_text
+        self.answer = q_answer
